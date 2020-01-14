9b0f26ceb9c2c7ea15232fa4e4f78360dd94c2e7
Stephanie Friend <stephaniefriend91@gmail.com>
2020-01-14 09:53:42 -0700

    Complete iteration 2

--- lib/market.rb
+++ lib/market.rb
@@ -6,4 +6,24 @@ class Market
     @name = name
     @vendors = []
   end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    @vendors.map do |vendor|
+      vendor.name
+    end
+  end
+
+  def vendors_that_sell(item)
+    @vendors.map do |vendor|
+      vendor.inventory.map do |ite, quant|
+        if ite == item
+          vendor
+        end
+      end
+    end.flatten.compact
+  end
 end
